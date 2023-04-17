5fd0f4f83a29a6fc0bd6ebe1b4fc317132107131
Moshe Rokach <mosherokach111@gmail.com>
2023-04-17 22:11:30 +0300

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import Store from "./app/Store";
+import store from "./app/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={Store}>
+  <Provider store={store}>
     <App />
   </Provider>
 );
