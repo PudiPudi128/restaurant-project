786529f82a5f8b49d6fcddba6d5e328019a3b48c
Moshe Rokach <mosherokach111@gmail.com>
2023-04-18 03:02:22 +0300

    secert button reset

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 import Home from "../components/Home";
 import Layout from "../layout/Layout";
 import NotFound404 from "../layout/NotFound404";
@@ -7,8 +7,12 @@ import RestaurantPage from "../components/RestaurantPage";
 import Addrestaurant from "../components/Addrestaurant";
 import ManageRestaurant from "../components/ManageRestaurant";
 import UpdateRestaurant from "../components/UpdateRestaurant";
+import { useDispatch } from "react-redux";
+import { setData } from "../features/data/DataSlice";
+import { restaurantData } from "../data/RestaurantsData";
 
 const AppRoutes = () => {
+  const dispatch = useDispatch();
   return (
     <Router>
       <Routes>
@@ -19,6 +23,7 @@ const AppRoutes = () => {
           <Route path="/restaurant/add" element={<Addrestaurant />} />
           <Route path="/restaurant/manage" element={<ManageRestaurant />} />
           <Route path="/restaurant/manage/update/:id" element={<UpdateRestaurant />} />
+          <Route path="/resetDB" element={<NavLink className="btn" to={"/"} onClick={() => dispatch(setData(restaurantData))}>resetDataBase</NavLink>} />
         </Route>
         <Route path="*" element={<NotFound404 />} />
       </Routes>
